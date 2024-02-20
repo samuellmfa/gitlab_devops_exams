fa1327b85461415c9fd8b019a5c69591a024b52a
Ubuntu <ubuntu@ip-172-31-7-117.eu-west-1.compute.internal>
2024-02-20 08:15:46 +0000

    files added

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "id": id})
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "WELCOME TO DATASCIENTEST MICRO SERVICE"}
+
+
+
+
